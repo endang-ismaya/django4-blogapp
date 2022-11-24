715a4e19af1432d5bf017708fe7fe72364e72bfe
Endang Ismaya <endang.ismaya@gmail.com>
2022-11-24 21:36:12 +0700

    add Tag Model

--- app_blog/models.py
+++ app_blog/models.py
@@ -1,12 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+# Tags
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True, db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        return super(Tag, self).save(*args, **kwargs)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     last_updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=200, unique=True, db_index=True)
     image = models.ImageField(null=True, blank=True, upload_to="images/")
+    tags = models.ManyToManyField(Tag, blank=True, related_name="post")
 
     def __str__(self) -> str:
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        return super(Post, self).save(*args, **kwargs)
