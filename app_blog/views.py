7e3a6c22ce75d65268985a4424de3963aa5085e1
Endang Ismaya <endang.ismaya@gmail.com>
2022-11-24 16:18:17 +0700

    add view::post_page and templates folder

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    ctx = {"post": post}
+    return render(request, "app_blog/post.html", ctx)
