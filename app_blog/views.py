6a20af8956649c177cc06075fd899c9883cde9a5
Endang Ismaya <endang.ismaya@gmail.com>
2022-11-26 22:40:57 +0700

    working with comments Part 4: update html for public-reply

--- app_blog/views.py
+++ app_blog/views.py
@@ -11,26 +11,45 @@ def post_page(request, slug):
         post = Post.objects.get(slug=slug)
         tags = post.tags.all()
         form = CommentForm()
-        comments = Comment.objects.filter(post=post)
+        comments = Comment.objects.filter(post=post, parent=None)
 
         if request.POST:
             comment_form = CommentForm(request.POST)
 
             if comment_form.is_valid():
-                comment = comment_form.save(
-                    commit=False
-                )  # not saving to db, get the instance of comment
-                comment.post = post
-                comment.save()
+                parent_obj = None
+                parent_id = request.POST.get("parent", -1)
+                if parent_id != -1:
+                    # save reply
+                    parent_obj = Comment.objects.get(pk=parent_id)
+
+                    if parent_obj:
+                        comment_reply = comment_form.save(commit=False)
+                        comment_reply.parent = parent_obj
+                        comment_reply.post = post
+                        comment_reply.save()
+
+                        return HttpResponseRedirect(
+                            reverse("app_blog:post_page", kwargs={"slug": slug})
+                        )
+                else:
+                    comment = comment_form.save(
+                        commit=False
+                    )  # not saving to db, get the instance of comment
+                    comment.post = post
+                    comment.save()
 
                 return HttpResponseRedirect(
                     reverse("app_blog:post_page", kwargs={"slug": slug})
                 )
 
-        # increment view_count
-        post.view_count = post.view_count + 1
-        # save to db
-        post.save()
+        else:
+            # increment view_count
+            post.view_count = post.view_count + 1
+            # save to db
+            post.save()
+
+        # return finally
         ctx = {"post": post, "tags": tags, "form": form, "comments": comments}
         return render(request, "app_blog/post.html", ctx)
     except:
